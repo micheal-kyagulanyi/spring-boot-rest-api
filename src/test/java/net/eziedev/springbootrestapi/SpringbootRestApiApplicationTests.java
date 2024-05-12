091b76ad8981250a50436590ffef4684257d0894
mkyagulanyi-nbcuni <michael.kyagulanyi@nbcuni.com>
2024-05-12 10:27:32 -0500

    Initial commit

---
+++ src/test/java/net/eziedev/springbootrestapi/SpringbootRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package net.eziedev.springbootrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
